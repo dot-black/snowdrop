3b28169393e2206f447bbaa7cba37942e83f876b
cupatea <vdavyden@gmail.com>
2017-05-15 21:22:21 +0300

    Add product

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ApplicationRecord
+  validates :title, :description, :image, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {
+    greater_than_or_equal_to: 0.01,
+    message:'incorrect, please enter a value at least greater then 0.01 or equal one'
+  }
+  validates :image, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+end
---
+++ test/models/product_test.rb
@@ -0,0 +1,68 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  
+  def new_product(image_url)
+    Product.new(
+      title: "My Product Title",
+      description: "My product description",
+      price: 22.99,
+      image: image_url
+    )
+  end
+
+    test "product attributes must not be empty" do
+      product = Product.new
+      assert product.invalid?
+      assert product.errors[:title].any?
+      assert product.errors[:description].any?
+      assert product.errors[:price].any?
+      assert product.errors[:image].any?
+    end
+
+    test "product price must be positive" do
+      product = self.new_product('image_url.jpg')
+      product.price = -1
+      assert product.invalid?
+      product.errors[:price]
+      product.price = 0
+      assert product.invalid?
+      product.errors[:price]
+      product.price = 1
+      assert product.valid?
+    end
+
+    test "image url" do
+      ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+      bad = %w{ fred.doc fred.gif/more fred.gif.more }
+      ok.each do |name|
+        assert new_product(name).valid?, "#{name} should be valid"
+      end
+      bad.each do |name|
+        assert new_product(name).invalid?, "#{name} shouldn't be valid"
+      end
+    end
+
+    test "product is not valid without a unique title" do
+      product = Product.new(
+        title: products(:iphone).title,
+        description: "The dublicated iPhone",
+        price: 12.44,
+        image: "copy.gif"
+      )
+      assert product.invalid?
+      assert_equal ["has already been taken"], product.errors[:title]
+    end
+
+    test "product is not valid without a unique title - i18n" do
+      product = Product.new(
+        title: products(:iphone).title,
+        description: "The dublicated iPhone",
+        price: 12.44,
+        image: "copy.gif"
+      )
+      assert product.invalid?
+      assert_equal [I18n.translate('errors.messages.taken')],
+      product.errors[:title]
+    end
+end
