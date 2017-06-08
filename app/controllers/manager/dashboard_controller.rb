84b9e1521028e581c5a3382413e9325580e38c3f
cupatea <vdavyden@gmail.com>
2017-06-09 00:10:22 +0300

    add statistics to manager  dashboard

--- app/controllers/manager/dashboard_controller.rb
+++ app/controllers/manager/dashboard_controller.rb
@@ -3,7 +3,20 @@ class Manager::DashboardController < ApplicationController
   layout 'managers/dashboard'
 
   def index
-    @products_count = Product.count
-    @orders_count = Order.count
+    @products_count = {
+      total: Product.count,
+      visible: Product.visible.count,
+      hiden: Product.hiden.count
+    }
+
+    @products_percentage = {
+      visible: ( @products_count[:visible].to_f / @products_count[:total].to_f * 100.0 ),
+      hiden:  ( @products_count[:hiden].to_f / @products_count[:total].to_f * 100.0 )
+    }
+
+    @products_by_categories_count = {}
+    Product.categories.each do |key, value|
+      @products_by_categories_count[key] = Product.public_send(key).count
+    end
   end
 end
