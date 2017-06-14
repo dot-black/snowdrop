6f6b5638bc21683e63a21a7c4a68be90dabd5004
cupatea <vdavyden@gmail.com>
2017-06-14 19:42:53 +0300

    Add special preview for manager

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,34 +1,37 @@
 class ProductsController < ApplicationController
   before_action :_set_product, only: :show
   before_action :_set_categories, only: [:index, :show]
-  before_action :_enusre_product_visible!, only: :show
 
 
   def index
-    @current_category = _current_category
-    @products = Product.shown.category(params[:category]).page params[:page] if _category_available?
+    redirect_to store_path unless @current_category = _current_category
+    @products =
+      manager_signed_in? ?
+        Product.relevant.category(params[:category]).page( params[:page] ) :
+        Product.shown.category(params[:category]).page( params[:page] )
   end
 
   def show
+    redirect_to products_path unless _enusre_product_and_category_visible!
   end
 
 
   private
 
   def _set_categories
-    @categories = Category.visible
+    @categories = manager_signed_in? ? Category.all : Category.visible
   end
 
   def _set_product
     @product = Product.find params[:id]
   end
 
-  def _enusre_product_visible!
-    redirect_to products_path unless @product.visible
+  def _enusre_product_and_category_visible!
+    manager_signed_in? or ( @product.visible and @product.category.visible )
   end
 
   def _category_available?
-    Category.exists?(params[:category]) and Category.find(params[:category]).visible
+    Category.exists?(params[:category]) and ( Category.find(params[:category]).visible or manager_signed_in? )
   end
 
   def _current_category
