0ca048fb8e921298633682408daacf97b8c563b6
cupatea <vdavyden@gmail.com>
2017-07-02 21:55:09 +0300

    Client system tests on headless chrome

--- test/system/line_items_test.rb
+++ test/system/line_items_test.rb
@@ -1,9 +1,51 @@
 require "application_system_test_case"
 
 class LineItemsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit line_items_url
-  #
-  #   assert_selector "h1", text: "LineItem"
-  # end
+  setup do
+    @category_first = categories(:first_visible)
+    @product_first = products(:iphone)
+    @product_second = products(:ipad)
+  end
+  test "check counters and total amount" do
+    visit store_url
+    #Go to first product
+    click_on @category_first.title.pluralize(2)
+    click_on @product_first.title
+    #2 times add first product
+    2.times do |time|
+      click_on 'Add to cart'
+      assert_selector ".cart-badge", text: "#{time + 1}"
+    end
+    #Go to second product
+    click_on @category_first.title.pluralize(2)
+    click_on @product_second.title
+    #Add second product with first size and check cart badge
+    click_on 'Add to cart'
+    assert_selector ".cart-badge", text: 3
+    #Add second product with second size and check cart badge
+    find('#line_item_size_standard').find(:xpath, 'option[2]').select_option
+    click_on 'Add to cart'
+    assert_selector ".cart-badge", text: 4
+    #Go to cart check line items count and total amount
+    visit cart_url
+    assert_selector "span", text: "(3 items)"
+    assert_selector "#cart-total-amount span strong", text: "Total: $#{(@product_first.price + @product_second.price) * 2}"
+    #Reduce amount of first product by 1 and check cart badge
+    find("##{@product_first.title}-regular").find('.minus-button').click
+    assert_selector ".cart-badge", text: 3
+    #Check total amount
+    assert_selector "#cart-total-amount span strong", text: "Total: $#{@product_first.price + 2 * @product_second.price}"
+    #Check if there is no link for reducing amount, when it equal to 1
+    find("##{@product_first.title}-regular").has_no_link? '.minus-button'
+    #Increase amount of first product by 1 and check cart badge
+    find("##{@product_first.title}-regular").find('.plus-button').click
+    assert_selector ".cart-badge", text: 4
+    #Check total amount
+    assert_selector "#cart-total-amount span strong", text: "Total: $#{(@product_first.price + @product_second.price) * 2}"
+    #Check if link for reduceing amount is appear
+    find("##{@product_first.title}-regular").has_link? '.minus-button'
+    #Check if checkout button is working
+    click_on 'Checkout'
+    assert_selector "h2", text: "Checkout"
+  end
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,8 +1,22 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit products_url
-    assert_selector "h1", text: "Product"
+  setup do
+    @category_first = categories(:first_visible)
+    @product_first = products(:iphone)
+    @product_second = products(:ipad)
+  end
+  test "Check product view" do
+    visit store_url
+    assert_selector "a", text: @category_first.title.pluralize(2)
+    click_on @category_first.title.pluralize(2)
+    assert_selector ".product-basic-title", text: @product_first.title
+    assert_selector ".product-basic-title", text: @product_second.title
+    assert_selector "a", text: @category_first.title.pluralize(2)
+    click_on @product_first.title
+    assert_selector "h1", text: @product_first.title
+    assert_selector "span", text: "$#{@product_first.price}"
+    assert_selector "select", text: @product_first.sizes.values.first.join(" ")
+    assert_selector "p", text: @product_first.description
   end
 end
