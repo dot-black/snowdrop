b15f540ace5f931da23f793966f5884bbdada61d
cupatea <vdavyden@gmail.com>
2017-07-12 22:08:44 +0300

    multiple images for manager; fix upload image bug; fix test images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def first_image product
     product.images == [] ?  "products/default.png" : product.images.first
   end
+
+  def category_image image, thumb = false
+    if image.present?
+      thumb ? image.thumb : image
+    else
+      thumb ? "categories/default_thumb.png" : "categories/default.png"
+    end
+  end
 end
--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -26,7 +26,7 @@ class ProductImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [400, 400]
+  process resize_to_fill: [1080, 1080]
   #
   # def scale(width, height)
   #   # do something
