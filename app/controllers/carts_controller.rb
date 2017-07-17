04b47390c8e230ca02b7ba0f9817fe45b7629890
verkligheten <min.verkligheten@gmail.com>
2017-07-17 23:19:22 +0300

    remove destroy cart action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,20 +1,10 @@
 class CartsController < ApplicationController
   include CurrentCart
-  before_action :_set_cart, only: [:show, :destroy]
+  before_action :_set_cart, only: :show
 
   def show
     _set_cart_line_items
     _set_cart_total_amount @line_items
     _set_cart_counter @line_items
-  end
-
-  def destroy
-    @cart.destroy if @cart.id == session[:cart_id]
-    session[:cart_id] = nil
-    respond_to do |format|
-      format.html { redirect_to store_url}
-      format.json { head :no_content }
-      format.js
-    end
   end
 end
