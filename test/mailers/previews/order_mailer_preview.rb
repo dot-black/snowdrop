70f2094c281004bb2df8830c3cb38d5f5e05318c
cupatea <vdavyden@gmail.com>
2017-07-29 10:53:09 +0300

    improve mailers

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -7,13 +7,13 @@ class OrderMailerPreview < ActionMailer::Preview
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/client_confirmation
-  def manager_information
-    OrderMailer.manager_information Order.first
-  end
-
-  # Preview this email at http://localhost:3000/rails/mailers/order_mailer/client_confirmation
   def client_confirmation
     OrderMailer.client_confirmation Order.first
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/order_mailer/manager_information
+  def manager_information
+    OrderMailer.manager_information Order.first
   end
 
 end
