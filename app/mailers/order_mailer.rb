907c520e1b3173b8fcbdf7969d427453173bb935
cupatea <vdavyden@gmail.com>
2017-08-29 21:29:19 +0300

    fix views

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -7,19 +7,19 @@ class OrderMailer < ApplicationMailer
   #
   def client_information order
     @order = order
-    mail to: order.email, subject: "ARI DAR LINGERIE order #{order.id}"
+    mail to: order.user.email, subject: "ARI DAR LINGERIE order #{order.id}"
   end
 
   def client_confirmation order
     @order = order
-    mail to: order.email,
+    mail to: order.user.email,
          subject: "ARI DAR LINGERIE order #{order.id}"
   end
 
   def manager_information order
     @order = order
     mailto = Rails.env.production? ?  ENV['GMAIL_NAME'] : 'aridar@example.com'
-    mail to: mailto,subject: "Order #{order.id} from #{order.name} / status: #{order.status}"
+    mail to: mailto,subject: "Order #{order.id} from #{order.user.name} / status: #{order.status}"
   end
 
 
