747fbeaba43b7d0aadbca77aeab8d5070550b9f9
cupatea <vdavyden@gmail.com>
2017-10-07 22:11:53 +0300

    add validation for telephone number

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -1,6 +1,6 @@
 class UserInformation < ApplicationRecord
   validates_presence_of   :telephone, :name
   validates :name, length: { maximum: 50 }
-  validates :telephone, length: { in: 9..20 }
+  validates :telephone, length: { in: 9..20 }, numericality: true
   belongs_to :user
 end
