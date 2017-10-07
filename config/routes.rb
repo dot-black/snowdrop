56cb9c118da592638e27d88234566569bc45c57f
cupatea <vdavyden@gmail.com>
2017-10-07 22:15:39 +0300

    fix missing template issue after post request

--- config/routes.rb
+++ config/routes.rb
@@ -38,13 +38,17 @@ Rails.application.routes.draw do
 
     root 'store#index', as: 'store'
     resources :line_items, only:[:create, :destroy, :update]
-    get :cart, to: 'carts#show'
-    resources :orders
     resources :products, only: [:show]
     get 'category/:category', to: 'products#index', as: 'category_products'
-    get :cart_items_count, to: "store#get_cart_items_count"
-    resources :users, only:[:new, :create]
-    resources :user_informations, only:[:new, :create]
+    get   :cart_items_count, to: "store#get_cart_items_count"
+
+    get   :cart,   to: 'carts#show'
+    get   :order,  to: 'orders#new',               as: 'new_order'
+    post  :order,  to: 'orders#create',            as: 'orders'
+    get   :client, to: 'users#new',                as: 'new_user'
+    post  :client, to: 'users#create',             as: 'users'
+    get   :info,   to: 'user_informations#new',    as: 'new_user_information'
+    post  :info,   to: 'user_informations#create', as: 'user_informations'
   end
   get '*path', to: redirect("/#{I18n.default_locale}/%{path}"), constraints: lambda { |req| !req.path.starts_with? "/#{I18n.default_locale}/" }
   get '', to: redirect("/#{I18n.default_locale}")
