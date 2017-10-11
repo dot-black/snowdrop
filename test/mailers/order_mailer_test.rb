8372d907114ed25a6daf9564267b9d47b9952d2b
cupatea <vdavyden@gmail.com>
2017-10-11 23:42:48 +0300

    fix client confirmation test

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -13,15 +13,6 @@ class OrderMailerTest < ActionMailer::TestCase
       # assert_equal ["from@example.com"], mail.from
       # assert_match "Hi", mail.body.encoded
     end
-
-    test "client_confirmation #{locale}" do
-      mail = OrderMailer.client_confirmation @order
-      assert_equal I18n.translate('order_mailer.client_confirmation.subject'), mail.subject
-      assert_equal [ @order.user.email ], mail.to
-      # assert_equal ["from@example.com"], mail.from
-      # assert_match "Hi", mail.body.encoded
-    end
-
     test "manager_information #{locale}" do
       mail = OrderMailer.manager_information @order
       assert_equal "New order ##{@order.id}", mail.subject
@@ -29,5 +20,13 @@ class OrderMailerTest < ActionMailer::TestCase
       # assert_equal ["from@example.com"], mail.from
       # assert_match "Hi", mail.body.encoded
     end
+  end
+  test "client_confirmation" do
+    I18n.locale  = @order.locale
+    mail = OrderMailer.client_confirmation @order
+    assert_equal I18n.translate('order_mailer.client_confirmation.subject'), mail.subject
+    assert_equal [ @order.user.email ], mail.to
+    # assert_equal ["from@example.com"], mail.from
+    # assert_match "Hi", mail.body.encoded
   end
 end
