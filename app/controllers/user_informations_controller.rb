983e70e2c879ceb0981270d0a0f2c1a203c02fc6
Vladyslav Davydenko <vdavyden@gmail.com>
2018-08-08 13:00:10 +0300

    rubocop refactoring user controllers

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -17,16 +17,14 @@ class UserInformationsController < StoreController
     end
   end
 
-private
-  def _set_user
-    @user = User.find_by_id session[:user_id]
-  end
+  private
 
   def _permitted_user_information_params
     params.require(:user_information).permit(:name, :telephone)
   end
 
   def _ensure_session_and_user_present
-    redirect_to new_user_path unless session[:user_id].present? and _set_user
+    @user = User.find_by_id session[:user_id]
+    redirect_to new_user_path unless @user
   end
 end
