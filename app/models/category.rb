d9b92d2626742b8d92ad5dd0c857d0af289a202e
Vladyslav Davydenko <vdavyden@gmail.com>
2018-08-10 01:39:42 +0300

    add translation and collbacks to category and product model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,12 @@
 class Category < ApplicationRecord
+  SLUG_REGEX = /\A[[a-z]\s\']+\z/
+  TRANSLATEABLE_ATTRBUTES = [:title].freeze
+
   include Translateable
-  translate [:title, :slug]
+  translate TRANSLATEABLE_ATTRBUTES
   has_many :translations, as: :translateable
   has_many :products, dependent: :destroy
   has_many :translations, as: :translateable
-
-  SLUG_REGEX = /\A[[a-z]\s\']+\z/
 
   mount_uploader :image, CategoryImageUploader
 
@@ -15,5 +16,8 @@ class Category < ApplicationRecord
   scope :visible, -> { where arel_table[:visible].eq true  }
   scope :hidden,  -> { where arel_table[:visible].eq false }
 
-  accepts_nested_attributes_for :translations
+  accepts_nested_attributes_for :translations, allow_destroy: true
+
+  after_create :add_translations
+
 end
