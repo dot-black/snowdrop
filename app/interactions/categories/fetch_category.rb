9cd029ec3f209061b4f17a43057671d008b6641e
Vladyslav Davydenko <vdavyden@gmail.com>
2018-08-09 21:57:27 +0300

    improve fetch category sevice

--- app/interactions/categories/fetch_category.rb
+++ app/interactions/categories/fetch_category.rb
@@ -4,6 +4,9 @@ class FetchCategory < ActiveInteraction::Base
   string :category_parameter
 
   def execute
-    Category.find_by_slug category_parameter.gsub("_", " ")
+    param = category_parameter.tr("_", " ")
+    Category.find_by(slug: param) ||
+      Translation.find_by(translateable_type: "Category", attribute_name: "slug", value: param)
+                 .try(:translateable)
   end
 end
